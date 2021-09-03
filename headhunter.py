e695932155acd6676ac9ba48ad4b790d3d0fe174
Roman Volodin <roman.volodin@gmail.com>
2021-09-03 22:31:56 +0300

    Add vacancies fetching for all language pages

--- headhunter.py
+++ headhunter.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 import requests
 
 
@@ -18,10 +20,15 @@ LANGUAGES = (
 )
 
 
-def fetch_language_vacancies(language):
+def fetch_language_vacancies(language, per_page=20, page=0):
     response = requests.get(
         "https://api.hh.ru/vacancies",
-        params={"area": AREAS["moscow"], "text": language},
+        params={
+            "area": AREAS["moscow"],
+            "text": language,
+            "per_page": per_page,
+            "page": page,
+        }
     )
     response.raise_for_status()
     return response.json()
@@ -66,3 +73,15 @@ def collect_languages_statistic(languages):
             "average_salary": average_salary
         }
     return languages_statictic
+
+
+def fetch_all_language_vacancies(language):
+    all_vacancies = []
+    for page in count():
+        vacancies = fetch_language_vacancies(
+            language, per_page=100, page=page
+        )
+        all_vacancies.extend(vacancies["items"])
+        if page == vacancies["pages"] - 1:
+            break
+    return all_vacancies
