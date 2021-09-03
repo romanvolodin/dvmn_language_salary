7cbe1e8809bff6eac0ce9ea9f75f025c3b73bd52
Roman Volodin <roman.volodin@gmail.com>
2021-09-04 00:05:45 +0300

    Add SuperJob salary calculation

--- superjob.py
+++ superjob.py
@@ -19,3 +19,17 @@ def fetch_language_vacancies(api_key, language, count=20, page=0):
     )
     response.raise_for_status()
     return response.json()
+
+
+def calc_rub_salary(vacancy):
+    if vacancy["currency"] != "rub":
+        return
+    min_salary = vacancy["payment_from"]
+    max_salary = vacancy["payment_to"]
+    if min_salary == 0 and max_salary == 0:
+        return
+    if min_salary == 0:
+        return max_salary * 0.8
+    if max_salary == 0:
+        return min_salary * 1.2
+    return (min_salary + max_salary) / 2
